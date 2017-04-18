dcab1061dbf056a952be3b4cc2bf3c8b61a19cdc
Matt Calthrop <mcalthrop@gmail.com>
2017-04-18 17:19:05 +0100

    Add test for add() method

--- spec/todos.controller.spec.js
+++ spec/todos.controller.spec.js
@@ -1,20 +1,44 @@
 describe('TodosController', () => {
-  var TodosController;
+  let TodosController;
+  let MockTodosFactory;
 
   beforeEach(() => {
     module('sampleApp');
 
+    MockTodosFactory = {
+      list: [],
+      add: jasmine.createSpy(),
+      clear: jasmine.createSpy()
+    };
+
     inject(($controller) => {
-      TodosController = $controller('TodosController');
+      TodosController = $controller('TodosController', { TodosFactory: MockTodosFactory });
     });
   });
 
-  describe('#init', () => {
+  describe('init()', () => {
     it('should set inputText correctly', () => {
       expect(TodosController.inputText).toEqual('');
     });
     it('should set list correctly', () => {
       expect(TodosController.list).toEqual([]);
+    });
+  });
+
+  describe('#add()', () => {
+    it('should call TodosFactory.add() with correct parameter', () => {
+      const newTodo = 'laze around';
+
+      TodosController.inputText = newTodo;
+      TodosController.add();
+      expect(MockTodosFactory.add).toHaveBeenCalledWith(newTodo);
+    });
+    it('should reset inputText', () => {
+      const newTodo = 'laze around';
+
+      TodosController.inputText = newTodo;
+      TodosController.add();
+      expect(TodosController.inputText).toEqual('');
     });
   });
 });
